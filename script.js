81e386eb24f21364905f0a4fe1f6e095ab1e2d4e
EDDIE MAROVICH <eddie.marovich@gmail.com>
2017-10-11 13:11:47 -0600

    red and black complete

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your JS goes here
+for(var i = 0; i <81; i++){
+var tile = document.createElement('div');
+var body = document.getElementsByTagName('body')[0];
+document.body.prepend(tile);
+tile.style.width = '11.1%'
+tile.style.paddingBottom = '11.1%'
+tile.display = "float"
+tile.style.float = "left"
+if(i%2 === 0){
+  tile.style.backgroundColor = "red";
+}else if (i % 2 === 1){
+  tile.style.backgroundColor = "black";
+}
+}
