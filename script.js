e0fd0e88a1867fc3243c88e8bfb7cfd1b52f0705
EDDIE MAROVICH <eddie.marovich@gmail.com>
2017-10-11 18:48:03 -0600

    gradient that covers the entire page

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var gradient = document.createElement('div');
-
-var body = document.getElementsByTagName('body')[0];
 
 for(var i = 0; i <81; i++){
   var tile = document.createElement('div');
+  var body = document.getElementsByTagName('body')[0];
 
   document.body.prepend(tile);
   tile.style.width = '11.1%'
@@ -11,23 +9,27 @@ for(var i = 0; i <81; i++){
   tile.display = "float"
   tile.style.float = "left"
   if(i%2 === 0){
-    tile.style.background = "linear-gradient(to top, green, purple)";
-    }else if (i % 2 === 1){
-    tile.style.background = "linear-gradient(to top, red, purple";
+    tile.style.backgroundColor = "black";
     }
+};
+var gradient = document.getElementsByTagName('body')[0];
+gradient.style.background = "linear-gradient(to right,green,teal)";
 
-}
-// gradient.style.top = "0";
-// gradient.style.bottom = "0";
-// gradient.style.left = '0';
-// gradient.style.right = '0';
-// gradient.style.backgroundImage =  'black';
-//
-// gradient.style.backgroundImage =  'linear-gradient (top, green, purple)';
-// gradient.style.position = 'absolute';
-//
-// document.body.append(gradient);
 
+// for(var i=0;i<81;i++){
+// var tile = document.createElement('div');
+// var body = document.getElementsByTagName('body')[0];
+// document.body.prepend(tile);
+// tile.style.width = '11.1%';
+// tile.style.paddingBottom = '11.1%';
+// tile.display = 'float';
+// tile.style.float = 'left';
+// if(i%2 === 0){
+// tile.style.backgroundColor = 'blue';
+// }
+// };
+// var doc = document.getElementsByTagName('body')[0];
+// doc.style.background = "linear-gradient(to right,green,teal,blue,indigo,violet, magenta)";
 
 
 // coll[i].style.background = "-moz-linear-gradient(top, #f1f471, #ffcc33)"
